a2e8de5a87e82ca889d7f1a1ec54bfb0df74b2cc
moojor224 <moojor224@gmail.com>
2025-07-08 14:29:05 -0500

    Bundle all-properties.en.json from w3.org https://www.w3.org/Style/CSS/all-properties.en.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,10 @@
+import fs from "fs";
+import path from "path";
 import { defineConfig } from "tsup";
+
+const props = await fetch("https://www.w3.org/Style/CSS/all-properties.en.json").then(e => e.json());
+const filepath = path.resolve(__dirname, "src", "css-properties.json");
+fs.writeFileSync(filepath, JSON.stringify(props), "utf-8");
 
 export default defineConfig({
     entry: ["index.ts"],
