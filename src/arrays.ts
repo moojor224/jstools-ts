1cde66108bac8d426b264ba9f144a3f5a1ff9543
moojor224 <moojor224@gmail.com>
2025-08-05 17:00:39 -0500

    add WIP jst_Array class

--- src/arrays.ts
+++ src/arrays.ts
@@ -80,3 +80,111 @@ export function interleaveArrays(fill: boolean, ...arrays: any[]): any[] {
 Array.prototype.unique = function () {
     return Array.from(new Set(this));
 }
+
+class jst_Array<T> {
+    private _arr: T[];
+    constructor(length: number);
+    constructor(...items: T[]);
+    constructor(...args: any[]) {
+        this._arr = new Array<T>(...args);
+        this[Symbol.iterator] = function* () {
+            for (var i = 0; i < this._arr.length; i++) {
+                yield this._arr[i];
+            }
+        }
+    }
+    static from(...args: Parameters<typeof Array.from>): jst_Array<any> {
+        const arr = Array.from(...args);
+        return new jst_Array().concat(arr);
+    }
+    static isArray(arg: any): arg is any[] {
+        return Array.isArray(arg);
+    }
+    static of(...args: Parameters<typeof Array.of>): ReturnType<typeof Array.of> {
+        return Array.of(...args);
+    }
+    get length() { return this._arr.length; }
+    toString(): string { return this._arr.toString(); }
+    toLocaleString(): string { return this._arr.toLocaleString(); }
+    pop(): T | undefined { return this._arr.pop(); }
+    push(...args: Parameters<InstanceType<typeof Array<T>>["push"]>): jst_Array<T> {
+        this._arr.push(...args);
+        return this;
+    }
+    concat(...items: Parameters<InstanceType<typeof Array<T>>["concat"]>): jst_Array<T> {
+        const newArr = this._arr.concat(...items);
+        const arr = new jst_Array<T>();
+        newArr.forEach(e => arr.push(e));
+        return arr;
+    }
+    join(separator?: string): string {
+        return this._arr.join(separator);
+    }
+    reverse(): jst_Array<T> {
+        this._arr.reverse();
+        return this;
+    }
+    shift(): T | undefined { return this._arr.shift(); }
+    slice(...args: Parameters<InstanceType<typeof Array<T>>["slice"]>): jst_Array<T> {
+        const newArr = this._arr.slice(...args);
+        const arr = new jst_Array<T>();
+        newArr.forEach(e => arr.push(e));
+        return arr;
+    }
+    sort(...args: Parameters<InstanceType<typeof Array<T>>["sort"]>): jst_Array<T> {
+        this._arr.sort(...args);
+        return this;
+    }
+    splice(...args: Parameters<InstanceType<typeof Array<T>>["splice"]>): jst_Array<T> {
+        this._arr.splice(...args);
+        return this;
+    }
+    unshift(...args: Parameters<InstanceType<typeof Array<T>>["unshift"]>): jst_Array<T> {
+        this._arr.unshift(...args);
+        return this;
+    }
+    indexOf(...args: Parameters<InstanceType<typeof Array<T>>["indexOf"]>): number { return this._arr.indexOf(...args); }
+    lastIndexOf(...args: Parameters<InstanceType<typeof Array<T>>["indexOf"]>): number { return this._arr.lastIndexOf(...args); }
+    every(...args: Parameters<InstanceType<typeof Array<T>>["every"]>) { return this._arr.every(...args); }
+    some(...args: Parameters<InstanceType<typeof Array<T>>["some"]>) { return this._arr.some(...args); }
+    forEach(...args: Parameters<InstanceType<typeof Array<T>>["forEach"]>): jst_Array<T> {
+        this._arr.forEach(...args);
+        return this;
+    }
+    map<K extends any>(callbackfn: (value: T, index: number, array: T[]) => K, thisArg?: any): jst_Array<K> {
+        const newArr = this._arr.map(callbackfn, thisArg);
+        const arr = new jst_Array<K>();
+        newArr.forEach(e => arr.push(e));
+        return arr;
+    }
+    filter(...args: Parameters<InstanceType<typeof Array<T>>["filter"]>): jst_Array<T> {
+        const newArr = this._arr.filter(...args);
+        const arr = new jst_Array<T>();
+        newArr.forEach(e => arr.push(e));
+        return arr;
+    }
+    reduce(...args: Parameters<InstanceType<typeof Array<T>>["reduce"]>) { return this._arr.reduce(...args); }
+    reduceRight(...args: Parameters<InstanceType<typeof Array<T>>["reduceRight"]>) { return this._arr.reduceRight(...args); }
+    [n: number]: T;
+    [Symbol.iterator] = function* () {
+        yield 1 as T;
+    };
+    [Symbol.unscopables] = {
+        at: true,
+        copyWithin: true,
+        entries: true,
+        fill: true,
+        find: true,
+        findIndex: true,
+        findLast: true,
+        findLastIndex: true,
+        flat: true,
+        flatMap: true,
+        includes: true,
+        keys: true,
+        toReversed: true,
+        toSorted: true,
+        toSpliced: true,
+        values: true,
+    }
+}
